b5aecdc02d8a70900f13a624ccc19424687794f1
bendoujanna <b.vitalina@alustudent.com>
2025-07-08 23:50:22 +0200

    has been changed

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,9 +3,9 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    best_key = None 
-    best_value = float('-inf') # Start with the lowest possible
-    
+    best_key = None
+    best_value = float('-inf')  # Start with the lowest possible
+
     for key in a_dictionary:
         if a_dictionary[key] > best_value:
             best_value = a_dictionary[key]
