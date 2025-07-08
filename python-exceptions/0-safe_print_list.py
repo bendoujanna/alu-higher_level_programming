ddec35c2c1b111be0b1604dd29f0f857f467cecb
bendoujanna <b.vitalina@alustudent.com>
2025-07-09 00:12:51 +0200

    had been changed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         while count < x:
             print(my_list[count], end="")
-            count += 1 
+            count += 1
     except IndexError:
         pass
     print()
