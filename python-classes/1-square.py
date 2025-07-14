0ab012bc70946eff241eb6b22a42084f08ea0803
bendoujanna <b.vitalina@alustudent.com>
2025-07-15 00:33:22 +0200

    sqare that has a private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a class Square with a private instance attribute"""
+
+
+class Square:
+    """Represents a square"""
+
+
+    def __init__(self, size):
+        """initializes the square with a given size"""
+        self.__size = size
+
