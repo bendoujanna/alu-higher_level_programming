daa17980c635a25ab6f5d5b46ef5aeaaf0ba3a32
bendoujanna <b.vitalina@alustudent.com>
2025-07-15 20:01:39 +0200

    has been changed

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
         return "\n".join(["#" * self.width for _ in range(self.height)])
 
     def __repr__(self):
-        """Return string representation able to recreate 
+        """Return string representation able to recreate
         a new instance via eval()
         """
         return f"Rectangle({self.width}, {self.height})"
