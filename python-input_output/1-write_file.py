70dfee929e38eede7874bdccc821e0356c9fcf0e
bendoujanna <b.vitalina@alustudent.com>
2025-07-17 16:20:42 +0200

    write a string to a textfile and returns the number of cha written

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Writes a string to a text file (UTF8) and returns 
+"""Writes a string to a text file (UTF8) and returns
 the number written
 """
 
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) 
+    Writes a string to a text file (UTF8)
     and returns the number of characters written.
 
     Args:
