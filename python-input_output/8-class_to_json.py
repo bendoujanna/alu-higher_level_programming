39ddfaabca41a5cf02231216b61bf2b5f3cf4642
bendoujanna <b.vitalina@alustudent.com>
2025-07-17 23:10:27 +0200

    returns the dict description of an for json serialization

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Returns the dictionary description of an object
+for JSON serialization.
+"""
+
+def class_to_json(obj):
+    """
+    Returns the dictionary representation of a simple data object.
+
+    Args:
+        obj: An instance of a class with serializable attributes.
+
+    Returns:
+        dict: The dictionary form of the object's attributes.
+    """
+    return obj.__dict__
