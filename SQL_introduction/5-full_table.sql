36420ae3747e7610e4af03a46b03831918a15145
bendoujanna <b.vitalina@alustudent.com>
2025-07-18 20:26:56 +0200

    get full description of the table first_table from information_schema

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- Get full description of the table first_table from information_schema
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.COLUMNS
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+-- Show the CREATE TABLE statement for first_table
+SHOW CREATE TABLE first_table;
