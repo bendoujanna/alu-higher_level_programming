6096f9dbac1c05dcd4df883d9242efd9490e2785
bendoujanna <b.vitalina@alustudent.com>
2025-07-18 08:41:31 +0200

    class Student that defines a student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,6 +3,7 @@
 Defines a Student class with filtering support in the to_json method.
 """
 
+
 class Student:
     def __init__(self, first_name, last_name, age):
         """
@@ -20,17 +21,13 @@ class Student:
     def to_json(self, attrs=None):
         """
         Returns a dictionary representation of a Student instance.
-        
         If attrs is a list of strings, only those attributes will be included.
         Otherwise, all attributes will be returned.
-
         Args:
             attrs (list or None): List of attribute names to retrieve.
-
         Returns:
             dict: Dictionary containing selected attributes or all if attrs is None.
         """
-        if isinstance(attrs, list) and all(type(attr) == str for attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
-        else:
-            return self.__dict__
+        if type(attrs) == list:
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+        return self.__dict__
