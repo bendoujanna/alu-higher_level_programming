2d73f00eacd4170ee32228dbf79df0e9504a32d7
bendoujanna <b.vitalina@alustudent.com>
2025-07-18 18:35:00 +0200

    pascal's triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle of size n.
+    Returns a list of lists of integers.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]  # Start with 1
+
+        for j in range(1, len(prev_row)):
+            # Add the sum of two elements from the previous row
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)  # End with 1
+        triangle.append(new_row)
+
+    return triangle
