da69d8dbb62f75983db72cf6c80cfb17ce5558f1
bendoujanna <b.vitalina@alustudent.com>
2025-07-26 23:51:41 +0200

    cURL to the end

---
+++ python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script sends a GET request to a given URL and displays the body only if the response status is 200.
+[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1"
