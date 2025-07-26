189a1a11dfec60cbd1ea4f17c56670a0722e77f0
bendoujanna <b.vitalina@alustudent.com>
2025-07-26 23:44:15 +0200

    Add a readme

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a GET request using curl, 
-# and displays the size of the response body in bytes.
-
-
+# This script takes a URL, sends a GET request using curl
 curl -sL "$1" | wc -c
