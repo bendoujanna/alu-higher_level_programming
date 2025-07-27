ebdf5a4b91077fe13640d5ee4e09765a486429d4
bendoujanna <b.vitalina@alustudent.com>
2025-07-27 23:04:11 +0200

    fetches

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
+
+import urllib.request
+
+url = "https://alu-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf-8"))
