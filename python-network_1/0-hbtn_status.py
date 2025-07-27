6d88a4ba7288165a710df092049681577600bcd7
bendoujanna <b.vitalina@alustudent.com>
2025-07-27 23:08:36 +0200

    Minor changes

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
+"""
+Fetches https://alu-intranet.hbtn.io/status using urllib
+and displays the response body in a specific format.
+"""
 
 import urllib.request
 
@@ -8,7 +11,7 @@ url = "https://alu-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     body = response.read()
 
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", body.decode("utf-8"))
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
